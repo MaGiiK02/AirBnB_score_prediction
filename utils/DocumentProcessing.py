034b7d87a9b69f0887d282687cc702f1ad97c1e9
angelinimattia <68020953+angelinimattia@users.noreply.github.com>
2023-04-08 12:33:39 +0300

    Added more token level functions, ad a fucntion to remove empty documents

--- utils/DocumentProcessing.py
+++ utils/DocumentProcessing.py
@@ -6,29 +6,13 @@ import itertools
 import re
 from collections import Counter
 
-
-
-
-def uncontract(text):    
-  text = re.sub(r"(\b)([Aa]re|[Cc]ould|[Dd]id|[Dd]oes|[Dd]o|[Hh]ad|[Hh]as|[Hh]ave|[Ii]s|[Mm]ight|[Mm]ust|[Ss]hould|[Ww]ere|[Ww]ould)n't", r"\1\2 not", text)
-  text = re.sub(r"(\b)([Hh]e|[Ii]|[Ss]he|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou)'ll", r"\1\2 will", text)
-  text = re.sub(r"(\b)([Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou)'re", r"\1\2 are", text)
-  text = re.sub(r"(\b)([Ii]|[Ss]hould|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Ww]ould|[Yy]ou)'ve", r"\1\2 have", text)
-  
-  text = re.sub(r"(\b)([Cc]a)n't", r"\1\2n not", text)
-  text = re.sub(r"(\b)([Ii])'m", r"\1\2 am", text)
-  text = re.sub(r"(\b)([Ll]et)'s", r"\1\2 us", text)
-  text = re.sub(r"(\b)([Tt]here)'s", r"\1\2 is", text)
-  text = re.sub(r"(\b)([Ww])on't", r"\1\2ill not", text)
-  text = re.sub(r"(\b)([Ss])han't", r"\1\2hall not", text)
-  text = re.sub(r"(\b)([Yy])(?:'all|a'll)", r"\1\2ou all", text)
-  
-  return text
-
-
-
+# Remove tokens that appear less than the lesser_eq_than between all the documetns
 def remove_rare_tokens(docs, lesser_eq_than):
     tokens_linear = itertools.chain.from_iterable(docs)
     token_freq = dict(Counter(tokens_linear))
     tokens_appeared_leq = set([t for t, v in token_freq.items() if v <= lesser_eq_than])
     return [list(filter(lambda t: t not in tokens_appeared_leq, doc)) for doc in docs]
+
+# Remove dockuments that have no tokens left
+def remove_empty(docs):
+    return list(filter(lambda d: len(d)>0, docs))
--- utils/TokenProcessing.py
+++ utils/TokenProcessing.py
@@ -2,15 +2,60 @@
 A collection of methods to work with tokens lists
 """
 import itertools
+import string
 import re
 from collections import Counter
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
 
+# Snipped to execute the download of nltk moule only if not present
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download(['wordnet', 'omw-1.4'])
+
+# Remplace urls and links tokens with the _URL placeholder
 def tokenise_URLS(tokens):
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
     url_regex = re.compile(url_regex)
     return [re.sub(url_regex, '_URL', t ) for t in tokens]
 
+# Remplace E-Mail tokens with the _EMAIL placeholder
 def tokenise_emails(tokens):
   email_regex = '^[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$'
   email_regex = re.compile(email_regex)
   return [re.sub(email_regex, '_EMAIL', t ) for t in tokens]
+
+
+# Remove all the punctuation only tokens
+def remove_puctuation_only(tokens):
+  return list(filter(lambda t: not all(c in string.punctuation for c in t), tokens))
+
+# Remove Numbers
+def remove_numbers(tokens):
+  return list(filter(lambda t: not t.isnumeric() , tokens))
+
+# remove the stop words from the tokens
+def remove_stop_words(tokens):
+  stop_word_regex = '|'.join(['^{}$'.format(s) for s in stopwords.words('english')])
+  stop_word_regex = re.compile(stop_word_regex)
+  return list(filter(lambda t: not stop_word_regex.match(t), tokens))
+
+# Some token are related to the domain this handcrafted function deal with removing them
+# DOMAIN_TOKENS =  ['@user']
+# def remove_domain_tokens(tokens):
+#   return list(filter(lambda t: t not in DOMAIN_TOKENS, tokens))
+
+# Lemmatixe the tokens using WordNetLemmatizer
+def lemmatize(tokens):
+  lemmatizer = WordNetLemmatizer()
+  return [lemmatizer.lemmatize(t) for t in tokens]
+
+# Stemmatize the token using PorterStemmer
+def stemmatize(tokens):
+  ps = PorterStemmer()
+  return [ps.stem(t) for t in tokens]
+
+# Remove empty tokens
+def filter_empty(tokens):
+  return list(filter(lambda t: t is not None and t != "", tokens))
