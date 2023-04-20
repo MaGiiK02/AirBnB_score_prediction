de70c53a48080c3b02a0ae71c92bc3ca60bd4cec
angelinimattia <68020953+angelinimattia@users.noreply.github.com>
2023-04-20 19:50:15 +0300

    Fixed the amenities code

--- processing/amenities.py
+++ processing/amenities.py
@@ -11,11 +11,9 @@ import itertools
 from scipy.cluster.hierarchy import ward, fcluster, linkage
 import gensim.downloader
 from sentence_transformers import SentenceTransformer, models
-from utils.textProcessing import tokenize, uncontract, lowercase
-from utils.tokenProcessing import remove_numbers, remove_puctuation_only, remove_stop_words, lemmatize
+from textProcessing import tokenize, uncontract, lowercase
+from tokenProcessing import remove_numbers, remove_puctuation_only, remove_stop_words, lemmatize
 from sklearn.metrics.pairwise import cosine_distances
-
-
 
 class AmenitiesClustering():
     """
@@ -34,7 +32,7 @@ class AmenitiesClustering():
     3. for each listing check for each cluster if it has an amenity near the cluster representation
     4. you get in output a list of array containing if the listing have an amenity in such cluster
     """
-    mProcessing = [
+    mClusteringProcessing = [
             remove_puctuation_only, 
             remove_numbers, 
             remove_stop_words, 
@@ -61,30 +59,45 @@ class AmenitiesClustering():
         tokens_flat = list(itertools.chain.from_iterable(tokens))
         unique_tokens = self.uniques(tokens_flat)
         vectorized_uniques_amenities = self.vectorize(unique_tokens)
-        self.mClusters = self.get_clusters_dict(vectorized_uniques_amenities)
+        vectorized_uniques_amenities = [v for v in vectorized_uniques_amenities if v.size == self.mEmbeddingSize]
+        clusters = self.extract_cluster(vectorized_uniques_amenities)
+        self.mClusters = self.get_clusters_dict(vectorized_uniques_amenities, clusters)
 
     def transform(self, df):
         tokens = self.to_tokens(df)
-        return self.extract_cluster_presence(tokens, self.mClusters)
+        return self.extract_cluster_presence(tokens)
     
     def fit_transform(self, df):
         tokens = self.to_tokens(df)
         tokens_flat = list(itertools.chain.from_iterable(tokens))
         unique_tokens = self.uniques(tokens_flat)
         vectorized_uniques_amenities = self.vectorize(unique_tokens)
-        self.mClusters = self.get_clusters_dict(vectorized_uniques_amenities)
-        return self.extract_cluster_presence(tokens, self.mClusters)
+        vectorized_uniques_amenities = [v for v in vectorized_uniques_amenities if v.size == self.mEmbeddingSize]
+        clusters = self.extract_cluster(vectorized_uniques_amenities)
+        self.mClusters = self.get_clusters_dict(vectorized_uniques_amenities, clusters)
+        return self.extract_cluster_presence(tokens)
     
     def extract_cluster_presence(self, tokenized_amenities):
-        return [self.extract_cluster_presence_single(a) for a in tokenized_amenities]
+        return [self.extract_cluster_presence_single(a_list) for a_list in tokenized_amenities]
 
     def extract_cluster_presence_single(self, tokenized_amenities):
         cluster_presence = np.zeros(len(self.mClusters))
+
+        cluster_map_pos = {}
+        for i, cluster in enumerate(self.mClusters):
+          cluster_map_pos[cluster] = i
+
         for amenities_list in tokenized_amenities:
-            encoded_amenities = self.w2v.most_similar(positive=amenities_list, topn=1)[0]
-            for idx, cluster_vector in self.mClusters:
-                distance = cosine_distances(encoded_amenities, cluster_vector)
-                cluster_presence[idx] = 1 if distance < self.mPresenceThreashold else 0
+            amenities_list = [self.model[a] for a in amenities_list if a in self.model]
+            if len(amenities_list) <= 0 : continue
+            # Retreive the word embedding by averaging the embedding of the ward composing the amenity
+            print(amenities_list[0].shape)
+            encoded_amenities = np.vstack(amenities_list).mean(axis=0)
+            for idx in self.mClusters:
+                cluster_vector = self.mClusters[idx]
+                distance = np.dot(encoded_amenities, cluster_vector) / (np.linalg.norm(encoded_amenities)*np.linalg.norm(cluster_vector))
+                pos = cluster_map_pos[idx]
+                cluster_presence[pos] = 1 if distance < self.mPresenceThreashold else 0
         return cluster_presence
         
     def get_clusters_dict(self, vectorized_tokens, clusters):
@@ -97,14 +110,15 @@ class AmenitiesClustering():
             if c not in cluster_words: cluster_words[c] = []
             cluster_words[c] +=[]
 
-        for idx,  c in cluster_dict:
-            cluster_dict[idx] = np.stack(c).mean(axis=1)
+        for idx in cluster_dict:
+            c = cluster_dict[idx]
+            cluster_dict[idx] = np.vstack(c).mean(axis=0)
 
         return cluster_dict
 
     def extract_cluster(self, vectorized_amenities):
         Z = linkage(vectorized_amenities,'average', metric='cosine')
-        clusters = fcluster(Z, threashold, criterion='distance')
+        clusters = fcluster(Z, self.mClusteringThreashold, criterion='distance')
         return clusters
 
     def tokenize(self, listings):
@@ -116,7 +130,7 @@ class AmenitiesClustering():
 
         return tokens_list
     
-    def uniques(amenities):
+    def uniques(self, amenities):
         res_list = []
         test = []
         for item in amenities: 
@@ -128,19 +142,19 @@ class AmenitiesClustering():
     
     def to_tokens(self, df):
         docs_tokens = self.tokenize(df)
-        docs_tokens = [self.apply_process_list(doc, self.mClusteringProcessing) for doc in docs_tokens]
+        docs_tokens = [self.apply_process_list(doc) for doc in docs_tokens]
         return docs_tokens
 
     def apply_process_list(self, tokens):
-        for p in self.mProcessing:
+        for p in self.mClusteringProcessing:
             tokens = [p(token) for token in tokens]
         return tokens
 
     def vectorize(self, docs):  
         documents_vector = []
         for d in docs:
-            vectorized_amenity_tokens = [self.w2v_model[t] for t in d if t in self.w2v_model]
-            documents_vector.append(np.zeros(shape=(self.mEmbeddingSize)) if len(vectorized_amenity_tokens) == 0 else np.mean(vectorized_amenity_tokens, axis=0))
+            vectorized_amenity_tokens = [self.model[t] for t in d if t in self.model]
+            if len(vectorized_amenity_tokens) >= 0: documents_vector.append(np.mean(vectorized_amenity_tokens, axis=0).astype(np.double))
         return documents_vector
 
 
